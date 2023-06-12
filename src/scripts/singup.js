6fc0ea32f2ae9309d17a93e88aa50785cdff5a5d
Miquéias Garcia <mglucenalucena@gmail.com>
2023-06-12 20:13:29 -0300

    Criando persistencia de usuario

--- src/scripts/singup.js
+++ src/scripts/singup.js
@@ -1,5 +1,29 @@
-async function singin(nome, email, senha) {
+async function verificarUsuario() {
+    let usuario = localStorage.getItem("usuarioId");
+
+    if(usuario) {
+        let dadosUsuario = await fetch(`http://10.0.0.124:3333/usuario/${usuario}`, {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json',
+        }
+        })
+
+        let status = dadosUsuario.status;
+        let dados = await dadosUsuario.json();
+
+        if(status == 200) {
+            localStorage.setItem("dadosUsuario", JSON.stringify(dados));
+            window.location.replace("http://10.0.0.124:5500/src");
+        } else if (status == 404) {
+            localStorage.removeItem("usuarioId");
+        }
+    }
+}
+
+async function singup(nome, email, senha) {
   let obj;
+  let status;
 
   const res = await fetch("http://10.0.0.124:3333/singup", {
       method: "POST",
@@ -14,9 +38,7 @@ async function singin(nome, email, senha) {
   })
 
   obj = await res.json();
-  console.log(res.status)
+  status = res.status;
 
-  console.log(obj);
-
-  return obj;
+  return [obj, status];
 }
