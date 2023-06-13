e86b55d8e67dcf1ad39a3fcb1d30da701e558fea
Miquéias Garcia <mglucenalucena@gmail.com>
2023-06-13 15:50:21 -0300

    Adicionando funcionalidade de logar

--- src/scripts/singin.js
+++ src/scripts/singin.js
@@ -2,7 +2,7 @@ async function singin(email, senha) {
   let obj;
   let status;
 
-  const res = await fetch(`${global.url}:3333/singin`, {
+  const res = await fetch(`${url}:3333/singin`, {
       method: "POST",
       body: JSON.stringify({
         email: email,
@@ -16,5 +16,27 @@ async function singin(email, senha) {
   obj = await res.json();
   status = res.status;
 
-  return [obj, status];
+  if(status == 200) {
+    localStorage.setItem("usuarioId", obj.idUsuario)
+
+    const dadosUsuario = await fetch(`${url}:3333/usuario/${obj.idUsuario}`, {
+      method: "GET",
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+
+    let statusBusca = dadosUsuario.status;
+    let dadosUsuarioJson = await dadosUsuario.json();
+
+    if(statusBusca == 200) {
+      localStorage.setItem("dadosUsuario", JSON.stringify(dadosUsuarioJson))
+
+      window.location.replace(`${url}:5500/src`);
+    }
+  } else if(status == 401) {
+    window.alert("Senha incorreta")
+  } else if(status == 404) {
+    window.alert("Email não existe")
+  }
 }
