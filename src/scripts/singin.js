f8705c9e0a347241ff4a2acd4cbc9066b32ef5db
Miquéias Garcia <mglucenalucena@gmail.com>
2023-06-13 10:01:02 -0300

    teste

--- src/scripts/singin.js
+++ src/scripts/singin.js
@@ -1,5 +1,29 @@
+async function verificarUsuario() {
+  let usuario = localStorage.getItem("usuarioId");
+
+  if(usuario) {
+      let dadosUsuario = await fetch(`http://localhost:3333/usuario/${usuario}`, {
+      method: "GET",
+      headers: {
+        'Content-Type': 'application/json',
+      }
+      })
+
+      let status = dadosUsuario.status;
+      let dados = await dadosUsuario.json();
+
+      if(status == 200) {
+        localStorage.setItem("dadosUsuario", JSON.stringify(dados));
+        window.location.replace("http://10.0.0.124:5500/src");
+      } else if (status == 404) {
+        localStorage.removeItem("usuarioId");
+      }
+  }
+}
+
 async function singin(email, senha) {
   let obj;
+  let status;
 
   const res = await fetch("http://10.0.0.124:3333/singin", {
       method: "POST",
@@ -13,9 +37,7 @@ async function singin(email, senha) {
   })
 
   obj = await res.json();
-  console.log(res.status)
+  status = res.status;
 
-  console.log(obj);
-
-  return obj;
+  return [obj, status];
 }
